f7e0ec3ec244ef24263672637fbae1ac158edebd
Simone Rossi <simone.rossi.93@gmail.com>
2017-05-15 12:51:17 +0200

    Added new version for the script

---
+++ assignment2/timing_simulation_SSEonly.sh
@@ -0,0 +1,47 @@
+make clean
+make CFLAGS=-O0
+
+rm sse_O0.csv
+touch sse_O0.csv
+
+for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288
+do
+  echo "Testing N = "$N
+  ./lab2 $N >> sse_O0.csv
+done
+
+make clean
+make CFLAGS=-O1
+
+rm sse_O1.csv
+touch sse_O1.csv
+
+for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288
+do
+  echo "Testing N = "$N
+  ./lab2 $N >> sse_O1.csv
+done
+
+make clean
+make CFLAGS=-O2
+
+rm sse_O2.csv
+touch sse_O2.csv
+
+for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288
+do
+  echo "Testing N = "$N
+  ./lab2 $N >> sse_O2.csv
+done
+
+make clean
+make CFLAGS=-O3
+
+rm sse_O3.csv
+touch sse_O3.csv
+
+for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288
+do
+  echo "Testing N = "$N
+  ./lab2 $N >> sse_O3.csv
+done

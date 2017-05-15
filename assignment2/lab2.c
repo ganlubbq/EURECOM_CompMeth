9659eee31fae084043a216c3fd3c40c9ab0b85ae
Simone Rossi <simone.rossi.93@gmail.com>
2017-05-15 10:38:12 +0200

    Added AVX2 multiplications

--- assignment2/lab2.c
+++ assignment2/lab2.c
@@ -1,45 +1,209 @@
 // Include files for SSE4
 #include "emmintrin.h"
 #include "xmmintrin.h"
+#include "time_meas.h"
+#include "smmintrin.h"
+#include <immintrin.h>
+
+
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// example SIMD macros, not necessary to be used, write your own
+#define ITER 1000000
 
-inline void cmult(__m128i a,__m128i b, __m128i *re32, __m128i *im32) {
 
-  mmtmpb    = _mm_sign_epi16(b,*(__m128i*)reflip);
-  *re32     = _mm_madd_epi16(a,mmtmpb);
-  mmtmpb    = _mm_shufflelo_epi16(b,_MM_SHUFFLE(2,3,0,1));
-  mmtmpb    = _mm_shufflehi_epi16(mmtmpb,_MM_SHUFFLE(2,3,0,1));
-  *im32  = _mm_madd_epi16(a,mmtmpb);
+void componentwise_multiply_real_scalar(int16_t *x,int16_t *y,int16_t *z, uint16_t N);
+void componentwise_multiply_real_sse4(int16_t *x, int16_t *y, int16_t *z, uint16_t N);
+void componentwise_multiply_real_avx2(int16_t *x, int16_t *y, int16_t *z, uint16_t N);
 
+int test_routines(uint16_t N, int seed);
+
+int main(int argc, char **argv) {
+
+    test_routines(atoi(argv[1]), 1234);
+
+    return 0;
 }
 
-__m128i cpack_tmp1,cpack_tmp2;
-inline __m128i cpack(__m128i xre,__m128i xim) {
-
-  cpack_tmp1 = _mm_unpacklo_epi32(xre,xim);
-  cpack_tmp1 = _mm_srai_epi32(cpack_tmp1,15);
-  cpack_tmp2 = _mm_unpackhi_epi32(xre,xim);
-  cpack_tmp2 = _mm_srai_epi32(cpack_tmp2,15);
-  return(_mm_packs_epi32(cpack_tmp1,cpack_tmp2));
-
+void print128_num(__m128i var)
+{
+    int16_t *val = (int16_t*) &var;
+    printf("%d %d %d %d %d %d %d %d \n",
+           val[0], val[1], val[2], val[3], val[4], val[5],
+           val[6], val[7]);
 }
 
-// routines to be written
-void componentwise_multiply_real_scalar(int16_t *x,int16_t *y,int16_t *z,uint16_t N) {
 
+void componentwise_multiply_real_scalar(int16_t *x,int16_t *y,int16_t *z, uint16_t N) {
+    int i;
+    for(i = 0; i < N; i+=8){
+        z[i] = x[i] * y[i];
+        z[i+1] = x[i+1] * y[i+1];
+        z[i+2] = x[i+2] * y[i+2];
+        z[i+3] = x[i+3] * y[i+3];
+        z[i+4] = x[i+4] * y[i+4];
+        z[i+5] = x[i+5] * y[i+5];
+        z[i+6] = x[i+6] * y[i+6];
+        z[i+7] = x[i+7] * y[i+7];
+    }
 }
-_
-// routines to be written
-void componentwise_multiply_real_sse4(int16_t *x,int16_t *y,int16_t *z,uint16_t N) {
 
-__m128i *x128 = (__m128i *)x;
-__m128i *y128 = (__m128i *)y;
-__m128i *z128 = (__m128i *)z;
- 
+void componentwise_multiply_real_sse4(int16_t *x, int16_t *y, int16_t *z, uint16_t N) {
+
+    __m128i *x128 = (__m128i *)x;
+    __m128i *y128 = (__m128i *)y;
+    __m128i *z128 = (__m128i *)z;
+
+    int i = 0;
+    for(i = 0; i < (N>>3); i+=8){
+        z128[i]   = _mm_mullo_epi16(x128[i],   y128[i]);
+        z128[i+1] = _mm_mullo_epi16(x128[i+1], y128[i+1]);
+        z128[i+2] = _mm_mullo_epi16(x128[i+2], y128[i+2]);
+        z128[i+3] = _mm_mullo_epi16(x128[i+3], y128[i+3]);
+        z128[i+4] = _mm_mullo_epi16(x128[i+4], y128[i+4]);
+        z128[i+5] = _mm_mullo_epi16(x128[i+5], y128[i+5]);
+        z128[i+6] = _mm_mullo_epi16(x128[i+6], y128[i+6]);
+        z128[i+7] = _mm_mullo_epi16(x128[i+7], y128[i+7]);
+    }
+    z = (int16_t*)z128;
 }
 
-main() {
+void componentwise_multiply_real_avx2(int16_t *x, int16_t *y, int16_t *z, uint16_t N) {
 
+    __m256i *x256 = (__m256i *)x;
+    __m256i *y256 = (__m256i *)y;
+    __m256i *z256 = (__m256i *)z;
+
+    int i = 0;
+    for(i = 0; i < (N>>4); i+=8){
+        z256[i]   = _mm256_mullo_epi16(x256[i],   y256[i]);
+        z256[i+1] = _mm256_mullo_epi16(x256[i+1], y256[i+1]);
+        z256[i+2] = _mm256_mullo_epi16(x256[i+2], y256[i+2]);
+        z256[i+3] = _mm256_mullo_epi16(x256[i+3], y256[i+3]);
+        z256[i+4] = _mm256_mullo_epi16(x256[i+4], y256[i+4]);
+        z256[i+5] = _mm256_mullo_epi16(x256[i+5], y256[i+5]);
+        z256[i+6] = _mm256_mullo_epi16(x256[i+6], y256[i+6]);
+        z256[i+7] = _mm256_mullo_epi16(x256[i+7], y256[i+7]);
+    }
+    z = (int16_t*)z256;
+}
+
+
+int test_routines(uint16_t N, int seed){
+
+    srand(seed);
+    time_stats_t time_stat;
+    int16_t *x, *y, *z_scal, *z_sse4, *z_avx2;
+
+    x = (int16_t*)malloc(sizeof(int16_t) * N);
+    y = (int16_t*)malloc(sizeof(int16_t) * N);
+    z_scal = (int16_t*)malloc(sizeof(int16_t) * N);
+    z_sse4 = (int16_t*)malloc(sizeof(int16_t) * N);
+    z_avx2 = (int16_t*)malloc(sizeof(int16_t) * N);
+
+    if (x == NULL || y == NULL || z_scal == NULL || z_sse4 == NULL || z_avx2 == NULL)
+        return -1;
+
+    int i;
+    for(i = 0; i < N; i++){
+        x[i] = 1;
+        y[i] = (int16_t)rand();
+    }
+
+    //printf("X = [ ");
+    //for(i = 0; i < N; i++){
+    //    printf("%d ", x[i]);
+    //}
+    //printf("]\n");
+
+    //printf("Y = [ ");
+    //for(i = 0; i < N; i++){
+    //    printf("%d ", y[i]);
+    //}
+    //printf("]\n");
+    #ifdef __DEBUG__
+    printf("Starting initial evaluation...\n");
+    #endif
+
+    componentwise_multiply_real_scalar(x, y, z_scal, N);
+    //printf("Z_scal = [ ");
+    //for(i = 0; i < N; i++){
+    //    printf("%d ", z_scal[i]);
+    //}
+    //printf("]\n");
+
+    componentwise_multiply_real_sse4(x, y, z_sse4, N);
+    //printf("Z_sse4 = [ ");
+    //for(i = 0; i < N; i++){
+    //    printf("%d ", z_sse4[i]);
+    //}
+    //printf("]\n");
+
+    componentwise_multiply_real_avx2(x, y, z_avx2, N);
+
+    for(i = 0; i < N; i++){
+        if(z_scal[i] != z_sse4[i] || z_scal[i] != z_avx2[i]) {
+            #ifdef __DEBUG__
+            printf("Inconsistent result in position %d\n Aborting!\n\n", i);
+            #endif
+            return -1;
+        }
+    }
+    #ifdef __DEBUG__
+    printf("Initial evaluation completed.\n");
+
+    printf("\nStarting scalar implementation evaluation...\n");
+    #endif
+
+    reset_meas(&time_stat);
+    for(i = 0; i < ITER; i++){
+        start_meas(&time_stat);
+        componentwise_multiply_real_scalar(x, y, z_scal, N);
+        stop_meas(&time_stat);
+    }
+    #ifdef __DEBUG__
+    printf("Diff..: %lld (ckt)\nMax...: %lld (ckt)\nTrials: %d (times)\nAvg...: %d\n",
+            time_stat.diff, time_stat.max, time_stat.trials, (int)(time_stat.diff/time_stat.trials));
+    #elif __PYTHON__
+    printf("Scalar, %d, %lld, %lld, %d, %d\n",
+            N, time_stat.diff, time_stat.max, time_stat.trials, (int)(time_stat.diff/time_stat.trials));
+    #endif
+
+    #ifdef __DEBUG__
+    printf("\nStarting SSE4 implementation evaluation...\n");
+    #endif
+    reset_meas(&time_stat);
+    for(i = 0; i < ITER; i++){
+        start_meas(&time_stat);
+        componentwise_multiply_real_sse4(x, y, z_sse4, N);
+        stop_meas(&time_stat);
+    }
+    #ifdef __DEBUG__
+    printf("Diff..: %lld (ckt)\nMax...: %lld (ckt)\nTrials: %d (times)\nAvg...: %d\n",
+            time_stat.diff, time_stat.max, time_stat.trials, (int)(time_stat.diff/time_stat.trials));
+    #elif __PYTHON__
+    printf("SSE4, %d, %lld, %lld, %d, %d\n",
+            N, time_stat.diff, time_stat.max, time_stat.trials, (int)(time_stat.diff/time_stat.trials));
+    #endif
+
+
+    #ifdef __DEBUG__
+    printf("\nStarting AVX2 implementation evaluation...\n");
+    #endif
+    reset_meas(&time_stat);
+    for(i = 0; i < ITER; i++){
+        start_meas(&time_stat);
+        componentwise_multiply_real_avx2(x, y, z_avx2, N);
+        stop_meas(&time_stat);
+    }
+    #ifdef __DEBUG__
+    printf("Diff..: %lld (ckt)\nMax...: %lld (ckt)\nTrials: %d (times)\nAvg...: %d\n",
+            time_stat.diff, time_stat.max, time_stat.trials, (int)(time_stat.diff/time_stat.trials));
+    #elif __PYTHON__
+    printf("SSE4, %d, %lld, %lld, %d, %d\n",
+            N, time_stat.diff, time_stat.max, time_stat.trials, (int)(time_stat.diff/time_stat.trials));
+    #endif
+
+    return 0;
 }

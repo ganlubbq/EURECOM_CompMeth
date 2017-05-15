93966701b73ecefdff502653b7760b5b2b80468a
Simone Rossi <simone.rossi.93@gmail.com>
2017-05-15 12:56:15 +0200

    Added results

--- assignment2/timing_simulation_AVX.sh
+++ assignment2/timing_simulation_AVX.sh
@@ -1,47 +1,47 @@
 make clean
-make CFLAGS=-O0 -mavx -mavx2
+make CFLAGS="-O0 -mavx -mavx2"
 
-rm sse_O0.csv
-touch sse_O0.csv
+rm avx_O0.csv
+touch avx_O0.csv
 
 for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288 1048576
 do
   echo "Testing N = "$N
-  ./lab2 $N >> sse_O0.csv
+  ./lab2 $N >> avx_O0.csv
 done
 
 make clean
-make CFLAGS=-O1 -mavx -mavx2
+make CFLAGS="-O1 -mavx -mavx2"
 
-rm sse_O1.csv
-touch sse_O1.csv
+rm avx_O1.csv
+touch avx_O1.csv
 
 for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288 1048576
 do
   echo "Testing N = "$N
-  ./lab2 $N >> sse_O1.csv
+  ./lab2 $N >> avx_O1.csv
 done
 
 make clean
-make CFLAGS=-O2 -mavx -mavx2
+make CFLAGS="-O2 -mavx -mavx2"
 
-rm sse_O2.csv
-touch sse_O2.csv
+rm avx_O2.csv
+touch avx_O2.csv
 
 for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288 1048576
 do
   echo "Testing N = "$N
-  ./lab2 $N >> sse_O2.csv
+  ./lab2 $N >> avx_O2.csv
 done
 
 make clean
-make CFLAGS=-O3 -mavx -mavx2
+make CFLAGS="-O3 -mavx -mavx2"
 
-rm sse_O3.csv
-touch sse_O3.csv
+rm avx_O3.csv
+touch avx_O3.csv
 
 for N in 16 32 64 128 256 512 1024 2048 4096 8192 16384	32768 65536 131072 262144 524288 1048576
 do
   echo "Testing N = "$N
-  ./lab2 $N >> sse_O3.csv
+  ./lab2 $N >> avx_O3.csv
 done

776a3207700474bfc833fdda827b6212c75718a6
Simone Rossi <simone.rossi.93@gmail.com>
2017-05-15 12:34:39 +0200

    Minor modifications

--- assignment2/lab2.c
+++ assignment2/lab2.c
@@ -24,7 +24,7 @@ void componentwise_multiply_real_avx2(int16_t *x, int16_t *y, int16_t *z, uint16
 #endif
 
 
-int test_routines(uint16_t N, int seed);
+int test_routines(uint32_t N, int seed);
 
 int main(int argc, char **argv) {
 
